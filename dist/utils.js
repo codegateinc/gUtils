43fdcbfb0f83a401a18d6c9875b308198b55d772
Grzegorz Wadas <wadas.grzegorz@o2.pl>
2020-02-11 16:14:08 +0100

    Added clearObject adn values

--- dist/utils.js
+++ dist/utils.js
@@ -5,3 +5,8 @@ export const T = () => {};
 export const hasKeys = subject => typeof subject === 'object' ? Object.keys(subject).length > 0 : false;
 export const toPairs = subject => Object.entries(subject);
 export const fromPairs = subject => Object.fromEntries(subject);
+export const isNil = subject => subject === null;
+
+const clearObject = subject => fromPairs(toPairs(subject).filter(([_, value]) => isDefined(value)));
+
+const values = subject => toPairs(subject).map(([_, value]) => value);
