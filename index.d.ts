3b336efa7fd29224b8b4591b0b81a0df9764aceb
Grzegorz Tarnopolski <tarnopolskigrzegorz@gmail.com>
2020-02-11 10:30:59 +0100

    added index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+export namespace G {
+  type KeyValuePair<T = any> = {
+    [key: string]: T
+  }
+
+  export function isDefined(subject: any): boolean
+  export function ifDefined<T = any>(subject: any, then: (subject: T) => void): T
+  export function call(fn: Function): void
+  export function T(): void
+  export function hasKeys(subject: any): boolean
+  export function toPairs<T extends {}>(subject: KeyValuePair): Array<[string, T]>
+  export function fromPairs<T extends {}>(subject: Array<[string, any]>): T
+}
