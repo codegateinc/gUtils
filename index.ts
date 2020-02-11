6fb5c5393abf3204bcf3087612ce5b104cba6fff
Grzegorz Tarnopolski <tarnopolskigrzegorz@gmail.com>
2020-02-11 09:40:23 +0100

    starter

---
+++ index.ts
@@ -0,0 +1,13 @@
+type KeyValuePair<T = any> = {
+  [key: string]: T
+}
+
+export const isDefined = (subject: any) => typeof subject !== 'undefined' && subject !== null
+export const ifDefined = <T = any>(subject: any, then: (subject: T) => void) => isDefined(subject) && then(subject)
+export const call = (fn: Function) => fn()
+export const T = () => {}
+export const hasKeys = (subject: any) => typeof subject === 'object'
+  ? Object.keys(subject).length > 0
+  : false
+export const toPairs = <T extends {}>(subject: KeyValuePair) => Object.entries<T>(subject)
+export const fromPairs = <T extends {}>(subject: Array<[string, any]>): T => Object.fromEntries(subject)
